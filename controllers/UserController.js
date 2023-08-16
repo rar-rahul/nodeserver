ccb73bdf17883a8ae28716bb61ea5a228bd84126
rar <rahulraut430@gmail.com>
2023-08-16 22:54:33 +0530

    login function changes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,15 +4,15 @@ const jwt = require("jsonwebtoken");
 const Users = require("../models/Users");
 
 exports.register = async (req, res) => {
+  //Find Email From Database which already exist
+  const emailExist = await Users.findOne({ email: req.body.email });
 
-     //Find Email From Database which already exist
-	const emailExist = await Users.findOne({ email:req.body.email});
-
-	//check if user already exist
-	if (emailExist) return res.status(400).json({ 
-			status:'fail',
-			message:'Email Already Exist'
-	});
+  //check if user already exist
+  if (emailExist)
+    return res.status(400).json({
+      status: "fail",
+      message: "Email Already Exist",
+    });
   //hash password using bcrypt
   let salt = await bcrypt.genSalt(10);
   let plaintext = req.body.password.toString();
@@ -25,57 +25,52 @@ exports.register = async (req, res) => {
     mobile: req.body.mobile,
   });
 
- // console.log(user)
+  // console.log(user)
 
   try {
     const newuser = await user.save();
     res.status(200).json({
       status: "success",
-      data: {newuser},
+      data: { newuser },
     });
   } catch (error) {
     res.status(400).json({
       status: "fail",
-      
     });
   }
 };
 
 exports.login = async (req, res) => {
-  
-    const user = await Users.findOne({email:req.body.email});
-    //check email is exist or not
-    if(!user){
-        res.status(400).json({
-            status:"fail",
-            message:"User Not Found"
-        })
-    }
-    //compare password and then check
-    const checkpass =  await bcrypt.compare(req.body.password,user.password);
-    if(!checkpass){
-        res.status(400).json({
-            status:"fail",
-            message:"Password Not Match"
-        })
-    }
-
-    //option expireIn object
-    const options = {
-        expiresIn: '1h' // Token expiration time
-      };
-
-    //sign in and genrate token
-    const token = jwt.sign({ _id: user._id },process.env.JWT_SECRET,options);
-    res.header('auth-token', token);
-
-    res.status(200).json({
-        success: 'success',
-        token,
-        user,
-        message:'Logged In !'
+  const user = await Users.findOne({ email: req.body.email });
+  //check email is exist or not
+  if (!user) {
+    res.status(400).json({
+      status: "fail",
+      message: "User Not Found",
     });
+  }
+  //compare password and then check
+  const checkpass = await bcrypt.compare(req.body.password, user.password);
+  if (!checkpass) {
+    res.status(400).json({
+      status: "fail",
+      message: "Password Not Match",
+    });
+  }
 
+  //option expireIn object
+  const options = {
+    expiresIn: "1h", // Token expiration time
+  };
 
+  //sign in and genrate token
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, options);
+  res.header("auth-token", token);
 
+  res.status(200).json({
+    success: "success",
+    token,
+    user,
+    message: "Logged In !",
+  });
 };
